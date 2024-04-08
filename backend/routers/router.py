62b4f716c8914c10b0405e767aae0205b80b56f8
Lex Jsk <anouxay.jsk@gmail.com>
2024-04-08 10:21:30 +0700

    init

--- backend/routers/router.py
+++ backend/routers/router.py
@@ -5,10 +5,9 @@ from routers.endpoints.user import router as user_router
 from routers.endpoints.user import router_token as user_router_token
 from routers.endpoints.predict import router as predict_router
 from routers.endpoints.predict import router_token as predict_router_token
-from routers.endpoints.report import router as report_router
 
 routers = APIRouter()
-router_list = [auth_router,user_router,user_router_token ,predict_router ,predict_router_token,report_router]
+router_list = [auth_router,user_router,user_router_token ,predict_router ,predict_router_token]
 
 for router in router_list:
     routers.include_router(router)
